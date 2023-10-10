1cfa77b689322e464fe5e01c3ff06eaed035af09
JAEKYUNG KIM <memo1239@naver.com>
2023-10-11 05:55:22 +0900

    Chapter 6.28

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,16 +1,7 @@
 import Video from "../models/Video";
-/*
-console.log("start")
-Video.find({}, (error, videos) => {
-  if(error){
-    return res.render("server-error")
-  }
-  return res.render("home", { pageTitle: "Home", videos });
-});
-console.log("finished")
-*/
+
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -35,16 +26,14 @@ export const getEdit = async (req, res) => {
 export const postEdit = async(req, res) => {
   const { id } = req.params;
   const {title, description, hashtags} = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({_id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title, 
     description, 
-    hashtags: hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -59,7 +48,7 @@ export const postUpload = async (req, res) => {
    await Video.create({
     title,
     description,
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect("/");
 } catch(error){
@@ -69,4 +58,24 @@ export const postUpload = async (req, res) => {
    });
 
 }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    // search
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
